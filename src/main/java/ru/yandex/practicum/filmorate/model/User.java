c7683b5172b25f46aa716dcd1a6bc83d1a9b4159
Frosty-WinPC <al.korobko@yahoo.com>
2025-02-25 21:32:29 +0100

    Правки для прохождения тестов - 3

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -25,6 +26,7 @@ public class User {
 
     // не требуется ли заодно и пароль, раз есть логин?
 
+    @Nullable
     String name;
 
     @NotNull
