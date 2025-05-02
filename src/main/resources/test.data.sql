b33b145d873a7f7ed1b000ab40d857f888a36d51
Frosty-MacOs <al.korobko@yahoo.com>
2025-05-02 20:34:02 +0200

    Вырезал справочную информацию из тестовых данных. Переименовал full.data.sql в test.data.sql

--- src/main/resources/test.data.sql
+++ src/main/resources/test.data.sql
@@ -1,18 +1,3 @@
-INSERT INTO rating (id, name) VALUES
-(1, 'G'),
-(2, 'PG'),
-(3, 'PG-13'),
-(4, 'R'),
-(5, 'NC-17');
-
-INSERT INTO genre (id, name) VALUES
-(1, 'Комедия'),
-(2, 'Драма'),
-(3, 'Мультфильм'),
-(4, 'Триллер'),
-(5, 'Документальный'),
-(6, 'Боевик');
-
 INSERT INTO director (id, name) VALUES
 (1, 'Christopher Nolan'),
 (2, 'Brosisters Wachowski'),
@@ -60,10 +45,6 @@ INSERT INTO film_director (film_id, director_id) VALUES
 (5, 1),
 (6, 3),
 (6, 4);
-
-INSERT INTO friendship_status (id, name) VALUES
-(1, 'UNCONFIRMED'),
-(2, 'CONFIRMED');
 
 -- если А дружит с Б, то пока Б не отправил запрос к А, статус дружбы "unconfirmed"
 INSERT INTO "friend" (user_id, friend_id, friendship_status_id) VALUES
