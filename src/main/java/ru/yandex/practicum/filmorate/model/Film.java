d6b4e5ff713217796468e23c804fa869b48f639f
Frosty-MacOs <al.korobko@yahoo.com>
2025-05-02 20:44:38 +0200

    Добавил @NotEmpty к полю Email у User

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,10 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.annotation.MinLocalDate;
@@ -29,7 +26,6 @@ public class Film {
 
     @NotNull
     @MinLocalDate
-    //@PastOrPresent может ли быть дата релиза в будущем?
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
 
